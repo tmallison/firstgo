7da4bd48578bfb15fae11119fb88cf02580c2988
Thomas <thomas68allison@hotmail.com>
2018-02-09 00:42:10 +0100

    Second method deck

--- cards/deck.go
+++ cards/deck.go
@@ -6,6 +6,21 @@ import "fmt"
 // wich is a slice of strings
 type deck []string
 
+func newDeck() deck{
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Heart", "Clubs"}
+	cardValue := []string{"One", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits{
+		for _, value := range cardValue{
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 func (d deck) print(){
 	for i, card := range d{
 		fmt.Println(i+1, card)
