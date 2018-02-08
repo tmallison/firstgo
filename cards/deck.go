cb52337c0d810dac56ea60b2a3814df503cfee81
Thomas <thomas68allison@hotmail.com>
2018-02-09 00:30:22 +0100

    Start deck

---
+++ cards/deck.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+// Create a new type of 'deck'
+// wich is a slice of strings
+type deck []string
+
+func (d deck) print(){
+	for i, card := range d{
+		fmt.Println(i+1, card)
+	}
+}
