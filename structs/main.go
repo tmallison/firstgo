b3c824e48659ec39c81fdf7e294c86e30460ed7a
Thomas <thomas68allison@hotmail.com>
2018-02-10 13:40:47 +0100

    Structs and pointers

--- structs/main.go
+++ structs/main.go
@@ -10,5 +10,14 @@ type person struct {
 func main() {
 	thomas := person{"Thomas", "Allison"}
 
+	thomasPointer := &thomas
+	thomasPointer.updateName("Tommy")
 	fmt.Println(thomas)
+
+	thomas.updateName("Tim")
+	fmt.Println(thomas)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
