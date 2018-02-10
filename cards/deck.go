122c213ebfdbfe87990ad32c4e9ffc8424785453
Thomas <thomas68allison@hotmail.com>
2018-02-10 11:20:20 +0100

    Random method and testing

--- cards/deck.go
+++ cards/deck.go
@@ -5,6 +5,8 @@ import (
 	"strings"
 	"io/ioutil"
 	"os"
+	"math/rand"
+	"time"
 )
 
 // Create a new type of 'deck'
@@ -53,4 +55,15 @@ func newDeckFromFile(filename string) deck {
 
 	s := strings.Split(string(bs), ",")
 	return deck(s)
+}
+
+func (d deck) shuffle() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		newPosition := r.Intn(len(d) - 1)
+
+		d[i], d[newPosition] = d[newPosition], d[i]
+	}
 }
--- cards/main.go
+++ cards/main.go
@@ -1,6 +1,7 @@
 package main
 
 func main()  {
-	cards := newDeckFromFile("myCards")
+	cards := newDeck()
+	cards.shuffle()
 	cards.print()
 }
